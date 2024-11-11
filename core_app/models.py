47aeb49e9ba8f629ec82439c7962842979c6d239
capiedrav <capiedrav@gmail.com>
2024-11-11 16:11:13 -0500

    Added custom user model

--- core_app/models.py
+++ core_app/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    """
+    Manager for users.
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create, save and return a new user.
+        """
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model.
+    """
+
+    email = models.EmailField(unique=True, max_length=255)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager() # assign UserManager class as the manager for User objects
+    
+    USERNAME_FIELD = "email" # use email field for authentication
